8e1e7b60ab770d4b061ceb90b5f8e454fae24201
Krzysztof <ki.zwolak@gmail.com>
2022-11-02 02:38:48 +0100

    Work on move changer

--- script.js
+++ script.js
@@ -22,13 +22,23 @@ gameBoard.forEach(cell => {
 
     //function that adds cell number to player array, turns
 
-    const turnCell = (p1, p2) => {
+    const turnMarker = (p1, p2) => {
         if (p1.turn == 0) {
 
         }
+       
     }
-    
-    cellDiv.addEventListener('click',);
+    const playerMove = (p1, p2) => {
+        let currentPlayer = p1;
+        currentPlayer.cells.push(cellDiv.textContent);
+        if (currentPlayer == p1) {
+            return currentPlayer = p2;
+        } else if (currentPlayer == p2) {
+            currentPlayer == p1
+        }
+    };
+
+    cellDiv.addEventListener('click', playerMove(p1, p2));
 
 
 });
@@ -44,13 +54,24 @@ const randomTurn = (p1, p2) => {
         p1.marker = "X";
         p2.turn = 1;
         p2.marker = "O";
-    }
-    else if (p1.turn < p2.turn) {
+    } else if (p1.turn < p2.turn) {
         p1.turn = 1;
         p1.marker = "O";
         p2.turn = 0;
         p2.marker = "X";
-    }
+    };
 };
 
 randomTurn(p1, p2);
+
+const cellContent = () => {
+    if (turn == 0) {
+        cellDiv.textContent = "O",
+        turn = 1;
+        cellDiv.removeEventListener('click', turnMarker);
+    } else if (turn == 1) {
+        cellDiv.textContent = "X",
+        turn = 0;
+        cellDiv.removeEventListener('click', turnMarker);
+    };
+};
