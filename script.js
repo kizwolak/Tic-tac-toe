638654093d9f3dc6a440bc3ce34972ce71e65cd6
Krzysztof <ki.zwolak@gmail.com>
2022-11-03 23:59:31 +0100

    Make win check return true

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ const gameplay = (() => {
             currentPlayer = p1;
         } else if (p1.turn < p2.turn) {
             return p1.marker = "X",
-            currentPlayer = p2,
-            p2.marker = "O";
+            p2.marker = "O",
+            currentPlayer = p2;
         };
     };
 
@@ -68,19 +68,29 @@ const gameplay = (() => {
             6: [2, 5, 8],
             7: [3, 6, 9],
             8: [3, 5, 7],
-            //One thing I just noticed, your win array has numbers and your takenCells are strings so they won't match when you do .includes.   
         };
-        let p1Cells = p1.takenCells;
-        let p2Cells = p2.takenCells;
-        for (let i = 1; i <= 8; i++) {
+        let p1Cells = p1.takenCells.map(Number);
+        let p2Cells = p2.takenCells.map(Number);
+
+        const isTruth = (i) => {
             return winArrays[i].every(cell => {
-                return(p1Cells.includes(cell));
+                return (p1Cells.includes(cell));
             });
         }
 
-        if (truthLoop == true) {
-            console.log("WIN!!")
+        const truthLoop1 = () => {for (let i = 1; i <= 8; i++) {
+            console.log(isTruth(i));
+            
+        }};
+
+        console.log(truthLoop1());
+
+        for (let i = 1; i <= 8; i++) {
+            return winArrays[i].every(cell => {
+                return(p2Cells.includes(cell));
+            });
         };
+        
     };
 
 
@@ -93,13 +103,5 @@ let cells = container.querySelectorAll('div');
 gameplay.randomTurn(p1, p2);
 gameplay.cellEvent();
 
-const winArrays = {
-    1: [1, 2, 3],
-    2: [4, 5, 6],
-    3: [7, 8, 9],
-    4: [1, 5, 9],
-    5: [1, 4, 7],
-    6: [2, 5, 8],
-    7: [3, 6, 9],
-    8: [3, 5, 7],
-};
+
+
